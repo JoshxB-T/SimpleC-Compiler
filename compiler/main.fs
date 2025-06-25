de610e9e95de9823f666c541fe4180eedcd12524
JoshxB-T <joshtorrez320@gmail.com>
2025-06-25 17:22:07 -0500

    Adding lexer code and updating codebase

---
+++ compiler/main.fs
@@ -0,0 +1,19 @@
+[<EntryPoint>]
+let main argv =
+  printf "SimpleC filename> "
+  let filename = System.Console.ReadLine()
+  printfn ""
+
+  if not (System.IO.File.Exists(filename)) then
+    printfn "**Error: file '%s' does not exist." filename
+    0
+  else
+    printfn "Compiling %s..." filename
+    let tokens = compiler.lexer.analyze filename
+    printfn ""
+    printfn "%A" tokens
+    printfn ""
+//    let result = compiler.parser.parse tokens
+//    printfn "%s" result
+//    printfn ""
+    0
