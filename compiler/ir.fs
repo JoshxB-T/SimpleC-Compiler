a8f2fd32d56d8a9da99fba659c7a7663dd801d73
JoshxB-T <joshtorrez320@gmail.com>
2025-07-10 22:03:58 -0500

    Expanded instruction type

--- compiler/ir.fs
+++ compiler/ir.fs
@@ -5,11 +5,17 @@ type var = string
 
 type Instruction =
   | LoadConst of Temp * int
-  | Assign of Var * Temp
+  | Assign of var * Temp
   | Add of Temp * Temp * Temp
   | Sub of Temp * Temp * Temp
   | Mul of Temp * Temp * Temp
   | Div of Temp * Temp * Temp
+  | Less of Temp * Temp * Temp
+  | Greater of Temp * Temp * Temp
+  | LessEq of Temp * Temp * Temp
+  | GreaterEq of Temp * Temp * Temp
+  | Equal of Temp * Temp * Temp
+  | NotEqual of Temp * Temp * Temp
 
 let mutable instructions : Instruction list = []
 let mutable tempCounter = 0
@@ -20,8 +26,12 @@ let newTemp () =
   t
 
 let emit instr =
-  instructions <- instructions @ [instr]
+  instructions <- instr :: instructions
+
+let getInstructions () =
+  List.rev instructions
 
 let reset () =
   instructions <- []
   tempCounter <- 0
+
